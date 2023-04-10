8093e3ac5f695d0792327dd2e8ea662d86bf3378
dKorch <paper_cup@qip.ru>
2023-04-10 21:57:12 +0300

    refactor

--- constants.js
+++ constants.js
@@ -5,6 +5,6 @@ const GONDON_BOT = "InYourPantsBot"
 const CALL_BROTHER = 'Я ща брата позову'
 
 // Пацанские цитаты.
-const QUOTES = ['То, что волк убил твоего недруга, не сделает волка твоим другом']
+const QUOTES = ['То, что волк убил твоего недруга, не сделает волка твоим другом', 'Мой статус прост: я лучше буду голодать, чем лакать из хозяйской миски.']
 
 module.exports = { GONDON_BOT, CALL_BROTHER, QUOTES }
--- index.js
+++ index.js
@@ -2,17 +2,25 @@ const { Telegraf } = require('telegraf')
 // Подключаем токен бота.
 const token = '6244052252:AAFR9SF4jiOZkGvM5X2wbZ07XjuNoBL0OXI';
 
+// Покдлючение модулей.
 const { GONDON_BOT, CALL_BROTHER, QUOTES } = require('./constants')
 const { QuoteRandom } = require('./quote_random')
 
+// Запускаем бота.
 const bot = new Telegraf(token)
+
+// Бот слушает брата.
 bot.hears(CALL_BROTHER, (ctx) => ctx.reply(QUOTES[0]))
+
+// Бот реагирует на пидорбота.
 bot.on('text', (ctx) => {
-    const chatId = ctx.message.chat.id
     const isShowdowns = ctx.message.reply_to_message?.from.username === GONDON_BOT
+    const message = QuoteRandom()
     
     if(isShowdowns) {
-        ctx.reply('Слышь, браток, не мороси.')
+        ctx.reply(message)
     }
 })
+
+
 bot.launch()
