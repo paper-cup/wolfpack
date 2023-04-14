463d73febbe865cd696896708cf7a83cdc4e6468
dKorch <paper_cup@qip.ru>
2023-04-14 15:52:27 +0300

    add Octokit

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const { Telegraf } = require('telegraf')
+const { Octokit } = require('@octokit/core')
 // Подключаем токен бота.
 const token = '6244052252:AAFR9SF4jiOZkGvM5X2wbZ07XjuNoBL0OXI';
+const authToken = 'Nr0qP0T9YOxTM9BQv+duKHe0wKcY59yd7lOQ6j87USs='
+
+// Octokit.js
+// https://github.com/octokit/core.js#readme
+const wolf = new Octokit({
+    auth: authToken
+  })
+  
+await wolf.request('GET /app/hook/config', {
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  })
 
 // Покдлючение модулей.
 const { GONDON_BOT, CALL_BROTHER, QUOTES, HELP } = require('./constants')
